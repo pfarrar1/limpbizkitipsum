6d40adbab7d1fc2b1c0076aa2340e61323da8249
Patrick Farrar <patrick@live-9.com>
2019-10-11 11:34:51 -0500

    add sass to nextjs

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import './header.scss'
+
+const Header = () => {
+    return(
+        <div className='header-container'>
+            <h1>Hello world header component</h1>
+        </div>
+    )
+}
+
+export default Header;
