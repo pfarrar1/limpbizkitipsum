0495967c61d430d1d3caf0fcb74ec3c65d171356
Mike Molenda <mike.molenda@rocketmiles.com>
2019-10-11 12:11:39 -0500

    #4: Move main container HTML to main componentï¿¿

---
+++ components/main/main.tsx
@@ -0,0 +1,15 @@
+import "./main.scss";
+
+const Main = () => {
+    return (
+        <div className="starter-template">
+            <h1>How Much Do You Want?</h1>
+            <button id="target" className="btn btn-lg btn-danger">
+                Click here
+            </button>
+            <div id="content"></div>
+        </div>
+    );
+};
+
+export default Main;
